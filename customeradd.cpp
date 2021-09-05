2619ca0634db160c6317ef9e9985445c7f12843e
Tsion Abebe <abebetsiyon47@gmail.com>
2021-09-05 16:14:38 +0300

    Update customeradd.cpp

--- customeradd.cpp
+++ customeradd.cpp
@@ -45,6 +45,26 @@ int customerCount = 1;
 Customer *customers = new Customer[customerCount];
 Customer *n_customers = new Customer[customerCount];
 
+void displaycustomers()
+{
+
+    cout << left << setw(3) << "No"
+         << setw(30) << "Name"
+         << setw(15) << "Age"
+         << setw(30) << "Phone"
+         << setw(10) << "Address"
+         << endl;
+    for (int i = 0; i < customerCount; i++)
+    {
+        cout << setw(3) << left << i + 1
+             << setw(30) << customers[i].name
+             << setw(15) << customers[i].age
+             << setw(30) << customers[i].phone
+             << setw(10) << customers[i].address
+             << endl;
+    }
+}
+
 void inputCustomer(Customer customers[], int i = 0)
 {
     cout << "Input Customer Info" << endl;
@@ -132,9 +152,116 @@ void addCustomers()
     }
 }
 
+void deletecustomers()
+{
+    displaycustomers();
+    int m, temp, n = 0;
+    cout << endl
+         << "Which Customer would you like to remove: ";
+    cin >> m;
+    m--;
+    temp = customerCount;
+    customerCount--;
+    delete[] n_customers;
+    n_customers = new Customer[customerCount];
+
+    cout << endl
+         << "first step pass"
+         << endl;
+
+    for (int z = 0; z < temp; z++)
+    {
+        if (z != m)
+        {
+            n_customers[n].name = customers[z].name;
+            n_customers[n].age = customers[z].age;
+            n_customers[n].phone = customers[z].phone;
+            n_customers[n].address = customers[z].address;
+            n_customers[n].username = customers[z].username;
+            n_customers[n].password = customers[z].password;
+            n++;
+        }
+    }
+
+    cout << "second step pass"
+         << endl;
+
+    delete[] customers;
+    customers = new Customer[customerCount];
+    for (int p = 0; p < customerCount; p++)
+    {
+        customers[p].name = n_customers[p].name;
+        customers[p].age = n_customers[p].age;
+        customers[p].phone = n_customers[p].phone;
+        customers[p].address = n_customers[p].address;
+        customers[p].username = n_customers[p].username;
+        customers[p].password = n_customers[p].password;
+    }
+
+    cout << "completed"
+         << endl;
+
+    cout << "Customer Successfully Removed, Going Back to Menu" << endl;
+    Sleep(2000);
+}
+void editCustomers()
+{
+    system("cls");
+    string choice;
+    cout << "VELCOME" << endl
+         << "1. Add customer" << endl
+         << "2. Delete customer" << endl
+         << "3. Show customers" << endl
+         << "4. Exit" << endl;
+    while (true)
+    {
+        cout << "Enter your choice: ";
+        cin >> choice;
+        if (floatCheck(choice))
+        {
+            if (choice == "1")
+            {
+                addCustomers();
+                system("cls");
+                editCustomers();
+                break;
+            }
+            else if (choice == "2")
+            {
+                system("cls");
+                deletecustomers();
+                displaycustomers();
+                Sleep(10000);
+                editCustomers();
+                break;
+            }
+            else if (choice == "3")
+            {
+                displaycustomers();
+                system("pause");
+                editCustomers();
+                break;
+            }
+            else if (choice == "4")
+            {
+                exit(0);
+                break;
+            }
+            else
+                cout << "Please Enter a Valid Number" << endl;
+            continue;
+        }
+        else
+        {
+            cout << "Please Enter a Number" << endl;
+            continue;
+        }
+    }
+}
+
 int main()
 {
-    //editCustomers();
+    editCustomers();
 
     return 0;
 }
